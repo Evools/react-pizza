2e2ca8daa1fc20445afdc39d694227eeb4954ab8
Abigor24 <jefr.p@yandex.ru>
2023-03-18 12:54:26 +0600

    create fetch responce

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import Categories from "./components/Categories";
 import Headers from "./components/Headers";
 import PizzaBlock from "./components/PizzaBlock";
 import Sort from "./components/Sort";
-import pizzas from './pizzas.json';
 import "./scss/app.scss";
+import { useState, useEffect } from 'react';
 
 
 function App() {
+  // https://641559b753d7f60a8cab69c0.mockapi.io/items
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://641559b753d7f60a8cab69c0.mockapi.io/items')
+      .then((res) => { return res.json(); })
+      .then((arr) => { setItems(arr); });
+    return () => {
+    };
+  }, []);
+
+
   return (
     <div className="wrapper">
       <Headers />
@@ -19,7 +32,7 @@ function App() {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {
-              pizzas.map(obj => <PizzaBlock key={ obj.id + Math.random() } { ...obj } />)
+              items.map(obj => <PizzaBlock key={obj.id + Math.random()} {...obj} />)
             }
           </div>
         </div>
